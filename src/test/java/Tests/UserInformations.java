56181ff4bb12df54a2a223eebbf0097f134c1470
Roehrs <roehrscontato@gmail.com>
2020-12-20 15:49:46 -0300

    add sign up and before/after

--- src/test/java/Tests/UserInformations.java
+++ src/test/java/Tests/UserInformations.java
@@ -1,14 +1,42 @@
 package Tests;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class UserInformations {
+    private WebDriver driver;
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\roehr\\IdeaProjects\\Drivers\\Chrome\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("http://www.juliodelima.com.br/taskit/");
+    }
     @Test
-    public void AddInformation(){
-        System.setProperty("webdriver.chrome.driver" , "C:\\Users\\roehr\\IdeaProjects\\Drivers\\Chrome\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+    public void AddInformation() {
+
+        driver.findElement(By.linkText("OK, I WANNA SIGN UP NOW")).click();
+
+        WebElement SignUpBox = driver.findElement(By.id("signupbox"));
+
+        SignUpBox.findElement(By.name("name")).sendKeys("Ana Merchury");
+
+        SignUpBox.findElement(By.name("login")).sendKeys("anamerchury5555@gmail.com");
+
+        SignUpBox.findElement(By.name("password")).sendKeys("ana5555@!");
+
+        SignUpBox.findElement(By.linkText("SAVE")).click();
+
+    }
+    @After
+    public void tearDown(){
+        driver.quit();
     }
 }
